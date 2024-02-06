a0ae3ee92fcaff82a8073216d7a21e918d9fac92
novikove <ekaterina.novikova@telefonica.com>
2024-02-06 16:08:11 +0100

    played with colors

--- src/components/HH.vue
+++ src/components/HH.vue
@@ -10,12 +10,8 @@
       </v-avatar>
     </v-col-4>
     <v-col cols="8" class="ml-12">
-      <h3 
-        class="font-weight-bold mb-2 neon-text-db">
-        Hamburg
-      </h3>
-      <p 
-        v-for="(fact, index) in hamburgFacts" 
+      <h3 class="font-weight-bold mb-2 neon-text-brown-lighten-3"> Hamburg </h3>
+      <!-- <p v-for="(fact, index) in hamburgFacts" 
         class="text-h5 font-weight-light mb-1 neon-text-p"  
         :key="index">
         {{ fact }}
@@ -28,17 +24,74 @@
         class="mb-4" 
         style="border-radius: 50%">
       </v-avatar>
+      </div> -->
+      <div>
+        <v-row>
+          <v-col v-for="card in hamburgFactsCards"
+            cols="12"
+            md="4"
+            :key="card.headline"
+          >
+            <v-card class="d-flex flex-column" :color="color" variant="outlined">
+              <v-card-item>
+                <div class="text-overline mb-1">
+                  {{ card.overline }}
+                </div>
+                <div :class="`text-h4 mb-1 font-weight-light neon-text-${color}`">
+                  {{ card.headline }}
+                </div>
+                <div class="text-h6 font-weight-light mb-1">
+                  {{ card.text }}
+                </div>
+              </v-card-item>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row align="center">
+          <v-col
+            cols="auto"
+          >
+            <v-radio-group
+              v-model="color"
+              inline
+            >
+              <v-radio
+                label="primary"
+                color="primary"
+                value="primary"
+              ></v-radio>
+              <v-radio
+                label="green-accent-2"
+                color="green-accent-2"
+                value="green-accent-2"
+              ></v-radio>
+              <v-radio
+                label="secondary"
+                color="secondary"
+                value="secondary"
+              ></v-radio>
+              <v-radio
+                label="brown-lighten-3"
+                color="brown-lighten-3"
+                value="brown-lighten-3"
+              ></v-radio>
+            </v-radio-group>
+          </v-col>
+        </v-row>
       </div>
     </v-col>
   </v-row>
 </template>
+
+<!-- beige? #bfb48a -->
+
 <!-- color: chartreuse -->
 
 <style>
 .hh-blue {
   color: #94bbfc
 }
-.neon-text-g {
+.neon-text-green-accent-2 {
   color: #fff; /* White text color */
   text-shadow:
     0 0 5px #c8e6c9, /* Inner light green glow */
@@ -63,7 +116,7 @@
   font-weight: bold;
   font-family: 'Arial', sans-serif;
 }
-.neon-text-db {
+.neon-text-brown-lighten-3 {
   color: rgb(148 187 252);
   text-shadow: 
     rgb(32 52 186) 0px 0px 5px, 
@@ -72,7 +125,7 @@
     rgb(4 29 147) 0px 0px 40px;
   font-family: "Rubik Scribble", system-ui;
 }
-.neon-text-p {
+.neon-text-primary {
   font-size: 36px;
   font-weight: bold;
   color: #f6e2ff;
@@ -81,7 +134,7 @@
     0 0 15px rgb(40, 40, 40, 10),
     0 0 15px rgba(238, 130, 238, 1);
 }
-.neon-text-v {
+.neon-text-secondary {
   color: rgb(213 172 254);
   text-shadow: 
     rgb(44 8 62) 0px 0px 5px,
@@ -93,7 +146,7 @@
 </style>
 
 <script setup>
-import { defineProps } from "vue";
+import { ref, defineProps } from "vue";
 
 // Define the props the component accepts
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -101,11 +154,23 @@ const props = defineProps({
   item: Object,
 });
 
+const variants = ["elevated", "flat", "tonal", "outlined"];
+const color = ref("primary");
+
 const hamburgFacts = [
   "- a city-state with more bridges than any other city in Europe (> 2,500)",
   "- has one of the largest and most acoustically advanced concert halls in the world (Elbphilarmonie)",
   "- 3rd largest port in Europe",
   "- the Hamburg America Line was the company that commissioned the construction of Titanic",
   "- celebrates the anniversary of the port's founding with ship parades and fireworks",
+];
+
+const hamburgFactsCards = [
+  {overline: "Germany", headline: "city-state", text: "enjoying a high degree of autonomy"},
+  {overline: "Bridges", headline: "> 2,500", text: "more than any other city in Europe"},
+  {overline: "Port", headline: "3rd", text: "largest in Europe (after Rotterdam and Antwerp)"},
+  {overline: "Hamburg America Line", headline: "Titanic", text: "the company that commissioned the construction"},
+  {overline: "Concert hall", headline: "Elbphilarmonie", text: "one of the largest and most acoustically advanced in the world"},
+  {overline: "Fireworks", headline: "Ship parade", text: "celebrates the anniversary of the port's founding"},
 ];
 </script>
