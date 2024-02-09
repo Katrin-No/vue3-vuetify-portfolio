b30df8e90ee6c86af83e86475521182737114fc0
novikove <ekaterina.novikova@telefonica.com>
2024-02-09 14:31:50 +0100

    forgot one

---
+++ src/reusable/TimeLine.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-timeline side="end">
+    <v-timeline-item v-for="dot in dots" :key="dot.id" :dot-color="dot.color" size="small">
+      <template v-slot:opposite>
+        <div :class="`pt-1 headline font-weight-bold text-${dot.color}`" v-text="dot.data"></div>
+      </template>
+      <div>
+        <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${dot.color}`">
+          {{ dot.title }}
+        </h2>
+        <div>{{ dot.text }}</div>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+  
+<script setup>
+import { defineProps } from "vue";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps({
+  dots: Object,
+});
+</script>
+  
