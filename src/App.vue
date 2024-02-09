990dfbae7417e2742d0803df461231ec7ba8bf6f
novikove <ekaterina.novikova@telefonica.com>
2024-02-09 10:00:20 +0100

    decompose tabs

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
-  <!-- <a href="#About2">gdfjgnr</a> -->
   <v-card>
-    <v-toolbar color="#87436a">
-      <v-toolbar-title>Portfolio</v-toolbar-title>
-    </v-toolbar>
+    <v-toolbar color="#87436a"><v-toolbar-title>Portfolio</v-toolbar-title></v-toolbar>
     <div class="d-flex flex-row">
-      <v-tabs v-model="tab" direction="vertical" color="cyan">
-        <v-tab v-for="item in navigationItems" :key="item.title" :value="item.value">
-          <v-icon left class="mr-1">{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-tab>
-      </v-tabs>
+      <MenuTabs :tabs="navigationItems" v-model="tab" />
       <v-window v-model="tab" direction="vertical">
-          <v-window-item v-for="item in navigationItems" :key="`window-${item.title}`" 
-            class="slide" :value="item.value">
-              <v-card flat>
-                <v-card-text>
-                  <component :is="item.component"/>
-                  
-                  <p v-if="item.text" class="neon-cyan">{{ item.text }}</p>
-                  <br/>
-                  <p class="mt-15" color="#18FFFF">
-                    Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor. Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor. Phasellus dolor. Fusce neque.
-                  </p>
-                </v-card-text>
-                <!-- <NeuroCalendar /> -->
-              </v-card>
-          </v-window-item>
-        </v-window>
+        <v-window-item v-for="item in navigationItems" :key="`window-${item.title}`" 
+          class="slide" :value="item.value">
+          <v-card flat>
+            <v-card-text>
+              <component :is="item.component"/>
+              <p class="mt-15" color="#18FFFF">{{ longText }}</p>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
     </div>
   </v-card>
 </template>
@@ -50,10 +36,10 @@ color="chartreuse"
 
 <script setup>
 import { ref } from "vue";
+import MenuTabs from "./reusable/MenuTabs.vue";
 import AvatarName from "./components/AvatarName.vue";
 import HH from "./components/HH.vue";
 import TimeLine from "./components/TimeLine.vue";
-// import SkillsStars from "./components/SkillsStars.vue";
 import SkillsSota from "./components/SkillsSota.vue";
 // import NeuroCalendar from "./components/NeuroCalendar.vue";
 import PTT from "./components/PTT.vue";
@@ -69,4 +55,6 @@ const navigationItems = ref([
 ]);
 
 const tab = ref("option-1");
+
+const longText = "Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor. Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu. Pellentesque libero tortor. Phasellus dolor. Fusce neque."
 </script>
---
+++ src/reusable/MenuTabs.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-tabs direction="vertical" color="cyan">
+    <v-tab v-for="tab in tabs" :key="tab.title" :value="tab.value">
+      <v-icon left class="mr-1">{{ tab.icon }}</v-icon>
+      {{ tab.title }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({
+  tabs: Object,
+});
+
+const tab = ref("option-1");
+</script>
