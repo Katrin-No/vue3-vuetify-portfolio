85341f9917a1e32eb42d146097db653f421f2543
novikove <ekaterina.novikova@telefonica.com>
2024-02-14 12:00:54 +0100

    add recepies

---
+++ src/components/RB.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row class="mr-8">
+    <v-col cols="12">
+      <h3 class="font-weight-bold mb-2 neon-text-brown-lighten-3"> Rezepten </h3>
+      <RadioButtons :radioButtons="radioButtons" v-model="selectedTag" @change="filterEntries()" />
+      <h2 class="mb-4">Einträge: {{ count }}</h2>
+      <v-row>
+        <v-col v-for="entry in filteredEntries" cols="12" md="2" :key="entry.id">
+          <v-card class="d-flex flex-column fill-height" rounded="shaped" :color="tagToColor[entry.tag]" variant="outlined">
+            <v-card-item>
+              <div class="text-overline mb-1">{{ entry.title }}</div>
+            </v-card-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import RadioButtons from "./../reusable/RadioButtons.vue";
+import recipies from "./recipies.js";
+
+const recipiesEntries = ref(recipies); 
+
+const radioButtons = [
+  { label: "Alle", value: "all", color: "deep-purple"},
+  { label: "Asiatisch", value: "Asiatisch", color: "orange"},
+  { label: "Gemüse", value: "Gemüse", color: "teal"},
+  { label: "Suppe", value: "Suppe", color: "yellow"},
+  { label: "Fisch", value: "Fisch", color:"blue"},
+  { label: "Russisch", value: "Russisch", color:"purple-accent-2"},
+  { label: "Fleisch", value: "Fleisch", color:"deep-orange"}
+];
+const selectedTag = ref("all");
+const tagToColor = {
+  "Asiatisch": "orange",
+  "Gemüse": "teal-accent-3",
+  "Suppe": "yellow",
+  "Fisch": "light-blue",
+  "Russisch": "purple-accent-2",
+  "Fleisch": "deep-orange"
+};
+
+const filteredEntries = ref([...recipiesEntries.value]);
+
+const randomOrder = recipiesEntries.value.sort(() => Math.random() - 0.5);
+
+const filterEntries = () => {
+  filteredEntries.value = selectedTag.value === "all" ? randomOrder
+    : recipiesEntries.value.filter(entry => entry.tag === selectedTag.value);
+};
+
+const count = computed(() => {
+  return filteredEntries.value.length;
+});
+</script>
---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="auto">
+    <v-card-item>
+      <h3>{{ randomItem.title }}</h3>
+      <v-card-subtitle>
+        <span class="me-1">{{randomItem.tag}}</span>
+        <v-icon color="success" icon="mdi-seed" size="small"></v-icon>
+      </v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      <v-row>
+        <v-col cols="3">
+          <div class="my-4 text-h5">Ingredients</div>
+          <div v-for="ingredient in randomItem.ingredients" :key="ingredient" class="text-h6 font-weight-light">
+            • {{ ingredient }}
+          </div>
+        </v-col>
+        <v-col cols="8">
+          <div v-if="randomItem.steps">
+            <div class="my-4 text-h5">Steps</div>
+            <v-row v-for="(step, stepIndex) in randomItem.steps" :key="stepIndex" class="flex-nowrap">
+              <v-chip variant="outlined" class="ma-2" color="green">{{ stepIndex + 1}}</v-chip>
+              <p class="text-h6 font-weight-light align-self-center">{{ step }}</p>
+            </v-row>
+            <div v-if="randomItem.notes" class="my-4 text-h5">Notes</div>
+            <ul v-for="note in randomItem.notes" :key="note" class="text-h6 font-weight-light">
+              <li> {{ note }}</li>
+            </ul> <!--  mdi-note-outline -->
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-divider class="mx-4 mb-1"></v-divider>
+  </v-card>
+</template>
+
+  
+<script setup>
+import { ref, defineProps, onMounted } from "vue";
+import recipies from "./recipies.js";
+
+const recipiesEntries = ref(recipies); 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps({
+  dots: Object,
+});
+
+const recipe = {
+  id: 24,
+  title: "Avocado-Wrap ⭐",
+  ingredients: [
+    "1 Dose(n) (à 425 ml) Kichererbsen",
+    "2 rote Paprikaschoten",
+    "1 Zucchini",
+    "150 g Kirschtomaten",
+    "1 rote Zwiebel",
+    "5 EL Olivenöl",
+    "Curry, Salz, Pfeffer",
+    "1/2 Limette",
+    "1 Avocado",
+    "1/2 Bund Koriander",
+    "1 Pck. Weizentortillas (8 Stück; 320 g)",
+  ],
+  steps: [
+    "Kichererbsen im Sieb abspülen, abtropfen lassen.",
+    "Zwiebel, Paprika und Zucchini klein würfeln. Tomaten halbieren.",
+    "4 EL Öl in einer Pfanne erhitzen. Paprika, Zucchini und Zwiebel darin ca. 3 Minuten braten.",
+    "1 EL Curry, Kichererbsen und Tomaten zugeben, ca. 2 Minuten weiterbraten. Mit Salz und Pfeffer würzen.",
+    "Guacamole zubereiten.",
+    "Tortillas in einer Pfanne ohne Fett von ­jeder Seite 10–20 Sekunden erhitzen. Mit Avocadomus, Gemüse und Koriander füllen und aufrollen.",
+  ],
+  tag: "Gemüse",
+};
+
+const randomItem = ref(recipe);
+// Select a random item when the component is mounted
+onMounted(() => {
+  const randomIndex = Math.floor(Math.random() * recipiesEntries.value.length);
+  randomItem.value = recipiesEntries.value[randomIndex];
+});
+</script>
+  
