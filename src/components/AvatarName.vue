f2ac9f1e3e8350a7faae52d906550d75a0b3f8d8
novikove <ekaterina.novikova@telefonica.com>
2024-02-16 14:57:17 +0100

    add neon corner

--- src/components/AvatarName.vue
+++ src/components/AvatarName.vue
@@ -1,13 +1,42 @@
 <template>
- <ImageCard image="cyber-ava.jpeg" size="160" />
-  <h1
-    class="font-weight-bold mt-9" 
-    style="font-family: fantasy; 
-    color: #b6f177">
-    Katrin
-  </h1>
+  <NeonCorner/>
+  <v-row>
+    <v-col>
+      <ImageCard image="cyber-ava.jpeg" size="160" class="neon-border mt-8" />
+      <h1
+        class="font-weight-bold mt-9 neon-green" 
+        style="font-family: fantasy;">
+        Katrin
+      </h1>
+    </v-col>
+    <v-col><v-img src="welcome.PNG" width="180" /> </v-col>
+  </v-row>
 </template>
+
+<style scoped>
+.neon-green{
+  color: #ffffffab;
+   text-shadow: 
+    0 0 5px #87f99a8c, 
+    0 0 8px #46b640cc, 
+    0 0 14px #37efa4, 
+    0 0 28px #3ca8829e; 
+}
+.neon-blue{
+  color: #ffffffab;
+  text-shadow: 
+  0 0 5px rgb(135 249 255 / 55%), 
+  0 0 12px rgb(70 171 255 / 80%), 
+  0 0 20px rgb(14 50 111), 
+  0 0 30px rgb(60 69 255 / 62%);
+}
+/* .neon-border {
+  border: 1px solid #614075db;
+  box-shadow: rgb(98 0 204 / 57%) 0px 0px 40px;
+} */
+</style>
 
 <script setup>
 import ImageCard from "./../reusable/ImageCard.vue";
+import NeonCorner from "./../reusable/NeonCorner.vue";
 </script>
---
+++ src/reusable/NeonCorner.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="neon-corner">
+    <div class="neon-h-line-g"></div>
+    <div class="neon-h-line-b"></div>
+    <div class="neon-v-line-g"></div>
+    <div class="neon-v-line-b"></div>
+  </div>
+</template>
+
+<style scoped>
+.neon-corner {
+  position: relative;
+  /* overflow: hidden; */
+}
+.neon-corner::before {
+  content: '';
+  position: absolute;
+  top: -25px;
+  right: -20px;
+  bottom: 16px;
+  left: -20px;
+  background-color: transparent;
+  border-radius: 10px;
+} 
+.neon-h-line-g, .neon-h-line-b, .neon-v-line-g, .neon-v-line-b {
+  position: absolute;
+  background-color: #fff;
+}
+.neon-h-line-b, .neon-v-line-b, .neon-corner::before {
+  box-shadow: 
+    0 0 10px #00ffea, 
+    0 0 20px #00ffea, 
+    0 0 40px #00ffea, 
+    0 0 80px #00ffea;
+}
+.neon-h-line-b {
+  top: 10px;
+  right: -8px;
+  width: 30vw;
+  height: 5px;
+}
+.neon-v-line-b {
+  top: -12px;
+  right: 25px;
+  width: 2px;
+  height: 20vw;
+}
+.neon-h-line-g, .neon-v-line-g {
+  box-shadow: 
+    0 0 8px #0f0, 
+    0 0 20px #0f0, 
+    0 0 40px #0f0;
+}
+.neon-h-line-g {
+  top: 0;
+  right: 0;
+  width: 30vw; 
+  height: 2px;
+}
+.neon-v-line-g {
+  top: 0;
+  right: 0;
+  width: 5px;
+  height: 20vw;
+}
+</style>
